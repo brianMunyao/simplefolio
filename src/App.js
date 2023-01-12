f715c4416345503a37c1019734f0e39732aafbbe
brianMunyao <brianmunyao6@gmail.com>
2023-01-12 17:25:44 +0300

    test: Update Main Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<Container>
+			<div className="top-part">
+				<h1>Hello , I'm Brian</h1>
+			</div>
+		</Container>
 	);
 }
+
+const Container = styled.div`
+	.top-part {
+		background: #2a9d8f;
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+`;
 
 export default App;
